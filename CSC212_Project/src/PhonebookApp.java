0cd731d9fb64468bfae77a83ce4b722fb7f0f835
windows 10 <windows 10@172.30.55.166>
2023-10-13 21:30:34 +0300

    added: -exceptions for special cases. -comments to clarify

--- CSC212_Project/src/PhonebookApp.java
+++ CSC212_Project/src/PhonebookApp.java
@@ -8,7 +8,7 @@ public class PhonebookApp {
 	static Scanner sc = new Scanner(System.in);
 
 	public static void main(String[] args) {
-		
+
 		//////////////////////////////////////////////////
 		int choice;
 		System.out.println("Welcome to the Linked Tree Phonebook!");
@@ -31,8 +31,7 @@ public class PhonebookApp {
 				sc.nextLine();
 
 				switch (choice) {
-				case 1:
-					// addContact();
+				case 1: //Add a contact.
 					System.out.print("\nEnter the contact's name: ");
 					String contactName = sc.nextLine();
 					System.out.print("Enter the contact's phone number: ");
@@ -45,51 +44,60 @@ public class PhonebookApp {
 					String birthday = sc.nextLine();
 					System.out.print("Enter any notes for the contact: ");
 					String notes = sc.nextLine();
-					
-					Contact c = new Contact(contactName,phoneNumber,emailAddress,address,birthday,notes);
-					if(p.addContact(c))
+
+					Contact c = new Contact(contactName, phoneNumber, emailAddress, address, birthday, notes);
+					if (p.addContact(c))
 						System.out.println("\n\nContact added successfully!\n");
 					else
 						System.out.println("\n\nContact is already in the contact list!\n");
 					break;
-				case 2:
-					// searchContact();
-					System.out.print("Enter search criteria: \n1.Name\n2.Phone Number\n3.Email Address\n4.Address\n5.Birthday\n\nEnter your choice: ");
-					int j = sc.nextInt();
-					sc.nextLine();
-					System.out.println();
-					switch(j) 
-					{
-					case 1:
-						System.out.println("Enter the Contact's name: ");
-						p.searchForContact(sc.next()+sc.nextLine(), j);
-						break;
-					case 2:
-						System.out.println("Enter the Contact's Phone number: ");
-						p.searchForContact(sc.next()+sc.nextLine(), j);
-						break;
-					case 3:
-						System.out.println("Enter the Contact's Email Address: ");
-						p.searchForContact(sc.next()+sc.nextLine(), j);
-						break;
-					case 4:
-						System.out.println("Enter the Contact's Address: ");
-						p.searchForContact(sc.next()+sc.nextLine(), j);
-						break;
-					case 5:
-						System.out.println("Enter the Contact's Birthday: ");
-						p.searchForContact(sc.next()+sc.nextLine(), j);
-						break;
-					default:
-						System.out.println("Wrong input!\n");
+				case 2: //Search for a contact.
+					int j = 0;
+					boolean flag = true;
+					while (flag) {
+						try {
+							System.out.print(
+									"\nEnter search criteria: \n1.Name\n2.Phone Number\n3.Email Address\n4.Address\n5.Birthday\n\nEnter your choice: ");
+							j = sc.nextInt();
+							sc.nextLine();
+							flag = false;
+							System.out.println();
+							switch (j) {
+							case 1: //Search by name
+								System.out.print("Enter the contact's name: ");
+								p.searchForContact(sc.next() + sc.nextLine(), j);
+								break;
+							case 2: //Search by phone number
+								System.out.print("Enter the contact's phone number: ");
+								p.searchForContact(sc.next() + sc.nextLine(), j);
+								break;
+							case 3: //Search by email address
+								System.out.print("Enter the contact's email Address: ");
+								p.searchForContact(sc.next() + sc.nextLine(), j);
+								break;
+							case 4: //Search by address
+								System.out.print("Enter the contact's address: ");
+								p.searchForContact(sc.next() + sc.nextLine(), j);
+								break;
+							case 5: //Search by birthday
+								System.out.print("Enter the contact's birthday: ");
+								p.searchForContact(sc.next() + sc.nextLine(), j);
+								break;
+							default:
+								System.out.println("Wrong input!");
+								flag = true;
+							}
+						} catch (InputMismatchException e) {
+							System.out.println("\nInvalid input!, Please enter a valid choice (1-5).");
+							sc.nextLine();
+						}
 					}
 					break;
-				case 3:
-					// deleteContact();
-					System.out.print("Enter the contact name to be deleted: ");
+				case 3: //Delete a contact.
+					System.out.print("Enter the contact's name to be deleted: ");
 					p.deleteContact(sc.nextLine());
 					break;
-				case 4:
+				case 4: //Schedule an event.
 					System.out.print("Enter event title: ");
 					String eventTitle = sc.nextLine();
 					System.out.print("Enter contact name: ");
@@ -98,50 +106,59 @@ public class PhonebookApp {
 					String date_time = sc.nextLine();
 					System.out.print("Enter event location: ");
 					String location = sc.nextLine();
-					Contact co = listC.searchContact(contName, 1);
-					if(co==null)
+					Contact contact = listC.searchContact(contName, 1);
+					if (contact == null)
 						System.out.println("Contact is not in the contact list!\n");
-					else 
-					{
-						Event e = new Event(eventTitle,date_time,location,co);
-						if(p.addEvent(e))
-							System.out.println("\nEvent added successfully!\n");
+					else {
+						Event e = new Event(eventTitle, date_time, location, contact);
+						if (p.addEvent(e))
+							System.out.println("\nEvent scheduled successfully!");
 						else
 							System.out.println("\nThere is a conflict in time with another Event.\n");
 					}
 					break;
-				case 5:
-					System.out.print("Enter search criteria: \n1.Contact Name\n2.Event title\n\nEnter your choice: ");
-					int i = sc.nextInt();
-					sc.nextLine();
-					if(i==1) 
-					{
-						System.out.print("\nEnter the contact name: ");
-						p.printEvent(sc.next()+sc.nextLine(), i);
-					}else if(i==2) 
-					{
-						System.out.print("\nEnter the event title: ");
-						p.printEvent(sc.next()+sc.nextLine(), i);
-					}else
-						System.out.println("\nPlease enter a valid choice (1-2).");
+				case 5: //Print event details.
+					boolean check = true;
+					while (check) {
+						try {
+							System.out.print(
+									"\nEnter search criteria: \n1.Contact name\n2.Event title\n\nEnter your choice: ");
+							int i = sc.nextInt();
+							sc.nextLine();
+							check = false;
+							if (i == 1) { //Search by contact name.
+								System.out.print("\nEnter the contact name: ");
+								p.printEvent(sc.next() + sc.nextLine(), i);
+							} else if (i == 2) {//Search by event title.
+								System.out.print("\nEnter the event title: ");
+								p.printEvent(sc.next() + sc.nextLine(), i);
+							} else {
+								System.out.println("\nPlease enter a valid choice (1-2).");
+								check = true;
+							}
+						} catch (InputMismatchException e) {
+							System.out.println("\nInvalid input!, Please enter a valid choice (1-2).");
+							sc.nextLine();
+						}
+					}
 					break;
-				case 6:
+				case 6: //Print contacts by first name.
 					System.out.print("\nEnter the first name: ");
 					p.printByFirstName(sc.next());
 					break;
-				case 7:
+				case 7: //Print all events alphabetically.
 					p.printAllEventsAlphabetically();
 					break;
-				case 8:
+				case 8: //Exit.
 					System.out.println("Exiting the program. Goodbye!");
 					break;
 				default:
-					System.out.println("Invalid choice. Please enter a valid option.\n");
+					System.out.println("\nInvalid choice, Please enter a valid choice (1-8).");
 				}
-			} catch (InputMismatchException e) { // Handle input mismatch exception
-				System.out.println("Invalid input. Please enter a valid choice (1-8).");
+			} catch (InputMismatchException e) { //Handle input mismatch exception
+				System.out.println("\nInvalid input, Please enter a valid choice (1-8).");
 				sc.nextLine();
-				choice = 0; // Reset choice
+				choice = 0; //Reset choice
 			}
 		} while (choice != 8);
 	}
