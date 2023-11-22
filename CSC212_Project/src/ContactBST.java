33f891f55d464d95ecf1e14dc75899e65bbfeb26
saleh <saleh@DESKTOP-D81CK7M>
2023-11-22 21:56:21 +0300

    Modified: Class Phonebook Class ContactBST Class PhonebookApp

--- CSC212_Project/src/ContactBST.java
+++ CSC212_Project/src/ContactBST.java
@@ -2,10 +2,8 @@ import java.util.Stack;
 
 public class ContactBST {
 	BSTNode root, current;
-	Boolean removed =false;
+	Boolean removed = false;
 
-
-	/** Creates a new instance of BST */
 	public ContactBST() {
 		root = current = null;
 	}
@@ -24,7 +22,7 @@ public class ContactBST {
 
 	public boolean findkey(String tkey) {
 		BSTNode p = root, q = root;
-		
+
 		if (empty())
 			return false;
 
@@ -42,14 +40,13 @@ public class ContactBST {
 		current = q;
 		return false;
 	}
-	
 
 	public boolean insert(Contact val) {
 		BSTNode p, q = current;
 
 		if (findkey(val.getName())) {
 			current = q;
-			return false; // key already in the BST
+			return false;
 		}
 
 		p = new BSTNode(val);
@@ -78,12 +75,12 @@ public class ContactBST {
 		if (p == null)
 			return null;
 		if (key.compareTo(p.key) > 0)
-			p.left = remove_aux(key, p.left); // go left
+			p.left = remove_aux(key, p.left);
 		else if (key.compareTo(p.key) < 0)
-			p.right = remove_aux(key, p.right); // go right
-		else { // key is found
+			p.right = remove_aux(key, p.right);
+		else {
 			removed = true;
-			if (p.left != null && p.right != null) { // two children
+			if (p.left != null && p.right != null) {
 				q = find_min(p.right);
 				p.key = q.key;
 				p.data = q.data;
@@ -109,28 +106,28 @@ public class ContactBST {
 		return p;
 	}
 
-	public boolean checkPN(BSTNode node, String condition) {	//Check if num is already exist
-        if (node != null) {
-            if (checkPN(node.left, condition)) 
-                return true;
-            if (node.data.getPhoneNumber().equals(condition)) {
-                return true; 
-                }
-            if (checkPN(node.right, condition)) 
-                return true; 
-        }
-        return false;
-    }
-	
+	public boolean checkPN(BSTNode node, String condition) { // Check if num is already exist
+		if (node != null) {
+			if (checkPN(node.left, condition))
+				return true;
+			if (node.data.getPhoneNumber().equals(condition)) {
+				return true;
+			}
+			if (checkPN(node.right, condition))
+				return true;
+		}
+		return false;
+	}
+
 	public Contact searchName(String tkey) {
 		BSTNode p = root, q = root;
-		
+
 		if (empty())
 			return null;
 
 		while (p != null) {
 			q = p;
-			if (tkey.compareTo(p.key) == 0) {
+			if (tkey.toLowerCase().compareTo(p.key.toLowerCase()) == 0) {
 				current = p;
 				return current.data;
 			} else if (tkey.compareTo(p.key) > 0)
@@ -142,80 +139,138 @@ public class ContactBST {
 		current = q;
 		return null;
 	}
-	
-	public boolean searchPhoneNumber(BSTNode node, String condition) {	
-        if (node != null) {
-            if (searchPhoneNumber(node.left, condition)) 
-                return true;
-            if (node.data.getPhoneNumber().equals(condition)) {
-            	System.out.println(node.data.toString());
-                return true; 
-                }
-            if (searchPhoneNumber(node.right, condition)) 
-                return true; 
-        }
-        return false;
-    }
-	
-	public boolean searchEmail(BSTNode root2, String Email) {
-		if (root2 == null) 
-			return false;
-		
-		Stack<BSTNode> stack = new Stack<>();
-		stack.push(root2);
 
-		while (!stack.isEmpty()) {
-			BSTNode current = stack.pop();
-			if (Email.equals(current.data.getEmail())) 
-				System.out.println(current.data.toString());
-			
-			if (current.left != null) 
-				stack.push(current.left);
-			 
-			if (current.right != null) 
-				stack.push(current.right);
-		}
-		return false;
-	}
-	public boolean searchAddress(BSTNode root2, String Address) {
-		if (root2 == null) 
-			return false;
-		
-		Stack<BSTNode> stack = new Stack<>();
-		stack.push(root2);
-
-		while (!stack.isEmpty()) {
-			BSTNode current = stack.pop();
-			if (Address.equals(current.data.getAddress())) 
-				System.out.println(current.data.toString());
-			
-			if (current.left != null) 
-				stack.push(current.left);
-			 
-			if (current.right != null) 
-				stack.push(current.right);
-		}
-		return false;
-	}
-	public boolean searchBirthday(BSTNode root2, String birthday) {
-		if (root2 == null) 
-			return false;
-		
-		Stack<BSTNode> stack = new Stack<>();
-		stack.push(root2);
-
-		while (!stack.isEmpty()) {
-			BSTNode current = stack.pop();
-			if (birthday.equals(current.data.getBirthday())) 
-				System.out.println(current.data.toString());
-			
-			if (current.left != null) 
-				stack.push(current.left);
-			 
-			if (current.right != null) 
-				stack.push(current.right);
+	public boolean searchPhoneNumber(BSTNode node, String phoneNumber) {
+		if (node != null) {
+			if (searchPhoneNumber(node.left, phoneNumber))
+				return true;
+			if (node.data.getPhoneNumber().equals(phoneNumber)) {
+				System.out.println("\nContact found!");
+				System.out.println(node.data.toString());
+				return true;
+			}
+			if (searchPhoneNumber(node.right, phoneNumber))
+				return true;
 		}
 		return false;
 	}
 
+	public int searchEmail(String Email) {
+		BSTNode root = this.root;
+		int flag = 0;
+		if (root == null)
+			return flag;
+
+		Stack<BSTNode> stack = new Stack<>();
+		stack.push(root);
+
+		while (!stack.isEmpty()) {
+			BSTNode current = stack.pop();
+			if (Email.equals(current.data.getEmail())) {
+				if (flag++ == 0)
+					System.out.println("\nContact found!");
+				System.out.println(current.data.toString());
+			}
+
+			if (current.left != null)
+				stack.push(current.left);
+
+			if (current.right != null)
+				stack.push(current.right);
+		}
+		return flag;
+	}
+
+	public int searchAddress(String Address) {
+		BSTNode root = this.root;
+		int flag = 0;
+		if (root == null)
+			return flag;
+
+		Stack<BSTNode> stack = new Stack<>();
+		stack.push(root);
+
+		while (!stack.isEmpty()) {
+			BSTNode current = stack.pop();
+			if (Address.equals(current.data.getAddress())) {
+				if (flag++ == 0)
+					System.out.println("\nContact found!");
+				System.out.println(current.data.toString());
+			}
+
+			if (current.left != null)
+				stack.push(current.left);
+
+			if (current.right != null)
+				stack.push(current.right);
+		}
+		return flag;
+	}
+
+	public int searchBirthday(String birthday) {
+		BSTNode root = this.root;
+		int flag = 0;
+		if (root == null)
+			return flag;
+
+		Stack<BSTNode> stack = new Stack<>();
+		stack.push(root);
+
+		while (!stack.isEmpty()) {
+			BSTNode current = stack.pop();
+			if (birthday.equals(current.data.getBirthday())) {
+				if (flag++ == 0)
+					System.out.println("\nContact found!");
+				System.out.println(current.data.toString());
+			}
+
+			if (current.left != null)
+				stack.push(current.left);
+
+			if (current.right != null)
+				stack.push(current.right);
+		}
+		return flag;
+	}
+
+	public int printFirstName(String firstName) {
+		BSTNode root = this.root;
+		int x = 0;
+		if (root == null)
+			return x;
+
+		Stack<BSTNode> stack = new Stack<>();
+		stack.push(root);
+
+		while (!stack.isEmpty()) {
+			BSTNode current = stack.pop();
+			String i = current.data.getName().split(" ")[0];
+			if (i.equalsIgnoreCase(firstName)) {
+				System.out.println(current.data.toString());
+				x++;
+			}
+
+			if (current.left != null)
+				stack.push(current.left);
+
+			if (current.right != null)
+				stack.push(current.right);
+		}
+		return x;
+	}
+
+	public void display(BSTNode p) {
+		if (p == null) {
+			return;
+		}
+
+		if (p.left != null) {
+			display(p.left);
+		}
+		System.out.print(p.data.getName() + ",");
+
+		if (p.right != null) {
+			display(p.right);
+		}
+	}
 }
